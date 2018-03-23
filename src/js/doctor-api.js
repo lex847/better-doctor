d9324328f1272cdab020219eb38082ef4f53d05e
Hannah Lee <hanlavlee@gmail.com>
2018-03-23 10:18:00 -0700

    create draft main.js and doctor-api.js syntax, and add html form.

--- src/js/doctor-api.js
+++ src/js/doctor-api.js
@@ -1,3 +1,18 @@
-export class Doctor {
+export class doctorAPI {
+  constructor(location) {
+    this.location = location;
+    // this.medicalIssue = medicalIssue;
+    // this.doctorName = doctorName;
+  }
+
+  getDoctor(location, displayDoctors) {
+    const apiKey = process.env.exports.apiKey;
+    $.get(`https://api.betterdoctor.com/2016-03-01/practices?location=${location}&skip=0&limit=10&${apiKey}=4c7deee526e1bdd9d202c9e28cd0d0ac`).then(function(response) {
+        displayDoctors(response);
+      })
+      .fail(function(error) {
+        $('.api-error').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+      });
+  }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
-import { Doctor } from './js/doctor-api.js'
+import { doctorAPI } from './js/doctor-api.js'
+
+const displayDoctors = function(response) {
+  if (response.meta.data.length == 0) {
+    console.log(response.meta.data.length);
+    $('.results').text("Sorry, there are no doctors that fit the search criteria.");
+  } else {
+    for (let i = 0; i < response.meta.data.length; i ++) {
+      let doctors = response.meta.data[i];
+      console.log(response.meta.data[i]);
+    $('.results').append(" " + '<li>' + doctors + '</li>');
+    }
+  }
+}
 
 $(document).ready(function() {
 
+  let newDoctorAPI = new doctorAPI(location);
+
+  $("#user-form").submit(function(event) {
+    event.preventDefault();
+
+    const location = $("#location").val();
+    $("#user-form").hide();
+
+    newDoctorAPI.getDoctor(location, displayDoctors);
+
+    $(".results").show();
+  })
 });
