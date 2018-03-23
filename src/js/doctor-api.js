f775d0e6ba91b72bce7765c844b1e7f3d08feeeb
Hannah Lee <hanlavlee@gmail.com>
2018-03-23 14:57:45 -0700

    hardkeyed in toothache to api to return data.

--- src/js/doctor-api.js
+++ src/js/doctor-api.js
@@ -7,7 +7,7 @@ export class doctorAPI {
   getDoctor(medicalIssue, displayDoctors) {
     const apiKey = process.env.exports.apiKey;
 
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${apiKey}`).then(function(response) {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response) {
         displayDoctors(response);
       })
       .fail(function(error) {
--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import { doctorAPI } from './js/doctor-api.js';
 import './styles.css' ;
 
 const displayDoctors = function(response) {
+  console.log(response);
   if (response.data.length == 0) {
     $('.results').text("Sorry, there are no doctors specializing in that area.");
     } else {
       for(let i = 0; i < response.data.length; i++) {
 
       let doctor = response.data[i].profile.first_name + " " + response.data[i].profile.last_name;
-      let newPatients = response.data[i].practices[i].accepts_new_patients;
 
-      $('.results').append(" " + '<li>' + doctor + " is " + newPatients + '</li>');
+
+      $('.results').append(" " + '<li>' + doctor + '</li>');
+    
     }
   }
 }
@@ -22,6 +24,7 @@ $(document).ready(function() {
     event.preventDefault();
 
     const medicalIssue = $("#medicalIssue").val();
+    console.log(medicalIssue);
     $("#user-form").hide();
 
     let newDoctorAPI = new doctorAPI(medicalIssue);
