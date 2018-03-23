8e68ccd72253ff867a17bb5ecc60cd8b7d3f4afa
Hannah Lee <hanlavlee@gmail.com>
2018-03-23 12:49:21 -0700

    keep returning CA not sure why..

--- src/js/doctor-api.js
+++ src/js/doctor-api.js
@@ -4,9 +4,22 @@ export class doctorAPI {
     // this.doctorName = doctorName;
   }
 
-  getDoctor(medicalIssue, displayDoctors) {
+  getDoctors(name, displayDoctors) {
+      const apiKey = process.env.exports.apiKey;
+
+      $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.523%2C-122.676%2C500&skip=0&limit=10&user_key=${apiKey}`).then(function(response)
+    {
+      displayDoctors(response);
+    })
+      .fail(function(error) {
+        $('.api-error').text(`There was an error processing your request: ${error.responseTest}. Please try again.`)
+      });
+    }
+
+  getSpecialties(medicalIssue, displayDoctors) {
     const apiKey = process.env.exports.apiKey;
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`).then(function(response) {
+
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?conditions=${medicalIssue}&location=45.523%2C-122.676%2C500&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${apiKey}`).then(function(response) {
         displayDoctors(response);
       })
       .fail(function(error) {
--- src/main.js
+++ src/main.js
@@ -2,28 +2,30 @@ import { doctorAPI } from './js/doctor-api.js'
 
 const displayDoctors = function(response) {
   if (response.data.length == 0) {
-    $('.results').text("Sorry, there are no doctors that fit the search criteria.");
+    $('.results').text("Sorry, there are no doctors specializing in that area.");
   } else {
-    for (let i = 0; i < response.data.practices.length; i ++) {
-      let doctors = response.data.practices.doctors[i];
-
+    for(let i = 0; i < response.data.length; i++) {
+    let doctors = response.data[i].profile;
+    let 
     $('.results').append(" " + '<li>' + doctors + '</li>');
-    }
   }
 }
+}
+
+
+
 
 $(document).ready(function() {
-
-  let newDoctorAPI = new doctorAPI();
-
   $("#user-form").submit(function(event) {
     event.preventDefault();
+
+    let newDoctorAPI = new doctorAPI(medicalIssue);
 
     const medicalIssue = $("#medicalIssue").val();
     $("#user-form").hide();
 
     newDoctorAPI.getDoctor(medicalIssue, displayDoctors);
 
-    $(".results").show();
+    // $(".results").show();
   })
 });
