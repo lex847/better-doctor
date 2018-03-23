2a612bf6348bffd853f252ea2ec83c8110f2bd00
Hannah Lee <hanlavlee@gmail.com>
2018-03-23 16:03:45 -0700

    create doctor name search, not yet working.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
 import { doctorAPI } from './js/doctor-api.js';
 import './styles.css' ;
 
-const displayDoctors = function(response) {
-  console.log(response);
+const displaySpecialty = function(response) {
   if (response.data.length == 0) {
     $('.results').text("Sorry, there are no doctors specializing in that area.");
     } else {
       for(let i = 0; i < response.data.length; i++) {
 
+      let doctor = response.data[i].profile.first_name + " " + response.data[i].profile.last_name + " " + response.data[i].profile[i].specialties;
+      console.log(response.data[i].profile[i].specialties);
+      let newPatients = response.data[i].practices[i].accepts_new_patients;
+
+      $('#results').append(" " + '<li>' + doctor + ": is accepting new patients: " + newPatients + '</li>');
+
+    }
+  }
+}
+
+const displayDoctors = function(response) {
+  if (response.data.length == 0) {
+    $('.results').text("Sorry, there are no doctors with that name.");
+    } else {
+      for(let i = 0; i < response.data.length; i++) {
+
       let doctor = response.data[i].profile.first_name + " " + response.data[i].profile.last_name;
+      let newPatients = response.data[i].practices[i].accepts_new_patients;
 
-
-      $('#results').append(" " + '<li>' + doctor + '</li>');
+      $('#results').append(" " + '<li>' + doctor + ": is accepting new patients: " + newPatients + '</li>');
 
     }
   }
@@ -24,12 +39,15 @@ $(document).ready(function() {
     event.preventDefault();
 
     const medicalIssue = $("#medicalIssue").val();
-    console.log(medicalIssue);
+    const doctorSearch = $("#doctorSearch").val();
+
     $("#user-form").hide();
 
     let newDoctorAPI = new doctorAPI(medicalIssue);
 
-    newDoctorAPI.getDoctor(medicalIssue, displayDoctors);
+    newDoctorAPI.getSpecialty(medicalIssue, displaySpecialty);
+
+    newDoctorAPI.getDoctorName(doctorSearch, displayDoctors);
 
     $("#results").show();
 
