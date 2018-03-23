fbb73b943e9206cdd1465f0d081e3330b999a334
Hannah Lee <hanlavlee@gmail.com>
2018-03-23 13:26:07 -0700

    api call still not working properly...

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@ import { doctorAPI } from './js/doctor-api.js'
 const displayDoctors = function(response) {
   if (response.data.length == 0) {
     $('.results').text("Sorry, there are no doctors specializing in that area.");
-  } else {
-    for(let i = 0; i < response.data.length; i++) {
-    let doctors = response.data[i].profile;
-    let 
-    $('.results').append(" " + '<li>' + doctors + '</li>');
+    } else {
+      for(let i = 0; i < response.data.length; i++) {
+
+      let doctor = response.data[i].profile.first_name + " " + response.data[i].profile.last_name;
+      let newPatients = response.data[i].practices[i].accepts_new_patients;
+
+      $('.results').append(" " + '<li>' + doctor + " is " + newPatients + '</li>');
+    }
   }
 }
-}
-
 
 
 
@@ -19,13 +20,12 @@ $(document).ready(function() {
   $("#user-form").submit(function(event) {
     event.preventDefault();
 
-    let newDoctorAPI = new doctorAPI(medicalIssue);
-
     const medicalIssue = $("#medicalIssue").val();
     $("#user-form").hide();
 
+    let newDoctorAPI = new doctorAPI(medicalIssue);
+
     newDoctorAPI.getDoctor(medicalIssue, displayDoctors);
 
-    // $(".results").show();
   })
 });
